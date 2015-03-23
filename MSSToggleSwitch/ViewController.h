4830792ad01020083980f1795160790246cbaebe
mbm29414 <michael@mustardseedsoftware.com>
2015-03-22 21:23:03 -0400

    Initial Commit

---
+++ MSSToggleSwitch/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MSSToggleSwitch
+//
+//  Created by Michael McEvoy on 3/22/15.
+//  Copyright (c) 2015 Mustard Seed Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MSSToggleSwitch/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MSSToggleSwitch
+//
+//  Created by Michael McEvoy on 3/22/15.
+//  Copyright (c) 2015 Mustard Seed Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
